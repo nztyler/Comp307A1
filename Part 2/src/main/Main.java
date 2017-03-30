5fe431c9c81b6021a2b78bccb0ae61de87c2a7bf
nztyler <nztyler@hotmail.co.nz>
2017-03-30 15:17:02 +1300

    Added part 2 to the repo

---
+++ Part 2/src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+/**
+ * Created by nztyler on 30/03/17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+}
